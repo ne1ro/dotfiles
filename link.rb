5a34184cf9b23c82eddeb69f042608336bf740c4
neiro <neiro.mail@gmail.com>
2019-12-14 20:34:42 +0300

    Revert "Remove tmux"
    This reverts commit 963622b39f021c1c251d48234da081fe8fefefd7.

--- link.rb
+++ link.rb
@@ -1,32 +1,3 @@
 #!/usr/bin/env ruby
 
-dotfiles_dir = Dir.pwd
-dotfiles = %w(
-  amethyst
-  curlrc
-  git_commit_msg.txt
-  gitconfig
-  gitignore_global
-  thymerc
-  tmux.conf
-  tmuxinator
-  zshrc
-  zshrc.local
-)
-
-# Link dotfiles
-dotfiles.each do |file|
-  linking_file = "#{ Dir.home }/.#{ file }"
-  linked_file = "#{ dotfiles_dir }/#{ file }"
-
-  if File.exist?(linking_file)
-    File.delete linking_file
-    puts "Remove existing file: #{ linking_file }"
-  end
-
-  File.symlink linked_file, linking_file
-  puts "Created symlink for .#{ file }", ''
-end
-
-File.delete("#{ Dir.home }/.config/nvim/init.vim")
-File.symlink("#{ dotfiles_dir }/init.vim", "#{ Dir.home }/.config/nvim/init.vim")
+File.symlink("~/dotfiles/init.vim", "#{ Dir.home }/.config/nvim/init.vim")
