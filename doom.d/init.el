f9e0d3126ef8634ef204a4225d6d5a132879efee
neiro <neiro.mail@gmail.com>
2020-09-03 23:53:49 +0200

    Make doom emacs faster by hiding numbers & idents

--- doom.d/init.el
+++ doom.d/init.el
@@ -32,7 +32,7 @@
        ;; fill-column       ; a `fill-column' indicator
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
-       indent-guides     ; highlighted indent columns
+       ;; indent-guides     ; highlighted indent columns
        (ligatures +fira)
        (modeline +light)          ; snazzy, Atom-inspired modeline, plus API
        nav-flash         ; blink the current line after jumping
